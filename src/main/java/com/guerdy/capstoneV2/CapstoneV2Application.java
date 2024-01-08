57b4c5c3a68e1e5fa2d2241f706513c9e64421c8
Guerdy Lambert <guerdylambert9@gmail.com>
2024-01-08 15:35:35 -0500

    capstone

---
+++ src/main/java/com/guerdy/capstoneV2/CapstoneV2Application.java
@@ -0,0 +1,13 @@
+package com.guerdy.capstoneV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/guerdy/capstoneV2/CapstoneV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guerdy.capstoneV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
